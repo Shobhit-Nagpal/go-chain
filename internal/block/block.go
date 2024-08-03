fdff53f981d4a8614ffb8160b19ab7b83536fff4
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-08-03 22:53:37 +0530

    add: create block

--- internal/block/block.go
+++ internal/block/block.go
@@ -1,17 +1,29 @@
 package block
 
 import (
-  "time"
-  "github.com/Shobhit-Nagpal/go-chain/internal/txn"
+	"github.com/Shobhit-Nagpal/go-chain/internal/txn"
+	"time"
 )
 
 type Metadata struct {
 	Timestamp   time.Time
 	BlockNumber int
-	Hash        string
+	PrevHash    string
 }
 
 type Block struct {
 	Metadata Metadata
 	Data     []txn.Txn
+}
+
+func CreateBlock(prevHash string) Block {
+	block := Block{
+		Metadata: Metadata{
+			Timestamp: time.Now(),
+			PrevHash:  prevHash,
+		},
+		Data: []txn.Txn{},
+	}
+
+	return block
 }
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/Shobhit-Nagpal/go-chain/internal/block"
+)
 
 func main() {
   fmt.Println("Hello world")
+  block := block.CreateBlock("lmfaoooo")
+  fmt.Println(block)
 }
